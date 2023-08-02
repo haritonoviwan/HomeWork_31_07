2d401633f7a39e4b2d5963d02656133580c66a0b
Иван Харитонов <haritonoviwan@yandex.ru>
2023-08-02 12:59:45 +0400

    Исправил 25 задание

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -4,9 +4,18 @@
 
 Console.Clear();
 int a = GetNumbersFromUser("Введите целое число A: ", "Ошибка ввода");
-int b = GetNumbersFromUser("Введите целое число B: ", "Ошибка ввода");
-int expNumbers = NumbersExponentiation(a, b);
-Console.WriteLine($"{a}, {b} -> {expNumbers}");
+int b = GetNumbersFromUser("Введите натуральное число B: ", "Ошибка ввода");
+int b_natural = NaturalNumber(b);
+
+int expNumbers = NumbersExponentiation(a, b_natural);
+Console.WriteLine($"{a}, {b_natural} -> {expNumbers}");
+
+int NaturalNumber(int num)
+{
+    if (num < 0)
+        num = -num;
+        return num;
+}
 
 int GetNumbersFromUser(string message, string errorMessage)
 {
@@ -22,10 +31,10 @@ int GetNumbersFromUser(string message, string errorMessage)
 
 int NumbersExponentiation(int firstNumber, int secondNumber)
 {
-    int exp = a;
-    for (int i = 1; i < b; i++)
+    int exp = firstNumber;
+    for (int i = 1; i < secondNumber; i++)
     {
-        exp = exp * a;
+        exp = exp * firstNumber;
     }
     return exp;
 }
